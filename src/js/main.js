dc1e32b500a6e6e276f31e746d7a9c1850391a22
SamiKamal <samixgames@hotmail.com>
2020-12-28 23:53:23 +0300

    Fixed issues & favicon

--- src/js/main.js
+++ src/js/main.js
@@ -1,106 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////// VARIABLES //////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 const rellax = require('rellax')
 const imgBack = document.querySelector('.img--back')
 const onView = document.querySelector('.onView')
 const downloadImg = document.querySelectorAll('.downloadApp__downloadImg')
-
-
-var rellaxx = new rellax('.rellax', {
-    round: true
-});
-
-
-window.addEventListener('scroll', e => {
-      if (isInViewport(onView)) {
-        imgBack.style.transform = 'translateY(-50%) rotate(0deg)'
-      } else {
-        imgBack.style.transform = 'translateY(-50%) rotate(15deg)'
-      }
-
-  })
-
-// scroll.on('call', func => {
-//     // Using modularJS
-//     if (func === 'imgBack'){
-//         imgBack.style.transform = 'translateY(-50%) rotate(0deg)'
-//     } else if (func === 'popUpDownload'){
-//         downloadImg.forEach(el => el.style.animationPlayState = 'running')
-//     }
-// });
-
-document.querySelector('.activeFAQ').style.paddingBottom = `${document.querySelector('.activeFAQ').children[1].children[0].offsetHeight + 120}px`
-document.querySelectorAll('.faq__card').forEach(el => {
-    el.style.paddingBottom = `${el.children[0].children[0].offsetHeight + 20}px`
-})
-document.querySelector('.faq__content').addEventListener('click', (e) => {
-    if (e.target.closest('.faq__card')){
-        Array.from(document.querySelector('.faq__content').children).forEach(el=>{
-            el.classList.remove('activeFAQ')
-            el.style.paddingBottom = `0`
-        })
-        e.target.closest('.faq__card').classList.add('activeFAQ')
-        document.querySelectorAll('.faq__card').forEach(el => {
-            el.style.paddingBottom = `${el.children[0].children[0].offsetHeight + 20}px`
-        })
-        document.querySelector('.activeFAQ').style.paddingBottom = `${document.querySelector('.activeFAQ').children[1].children[0].offsetHeight + 120}px`
-        
-    }
-    console.log(document.querySelector('.activeFAQ').children[1].children[0].offsetHeight)
-})
-
-// EXPREMENTIAL
 const card = document.querySelector('.plans__card--important')
 const container = document.querySelector('.container')
-//items
 let price = document.getElementById('plans__card--price')
 let title = document.getElementById('plans__card--mainText')
 let button = document.getElementById('plans__card--button')
 let features = document.getElementById('plans__card--features')
 let shakeOnClick = document.querySelectorAll('.shakeOnClick');
 let navList = document.querySelector('.nav__ul');
-
-shakeOnClick.forEach(el => {
-    el.addEventListener('click', e => {
-        el.classList.add('errorShake')
-        setTimeout(() => {
-            el.classList.remove('errorShake')
-        }, 600);
-    })
-    
-})
-
-// mouse animation
-container.addEventListener('mousemove', (e) => {
-    let xAAxis = -(window.innerWidth / 2 - e.pageX) / 30;
-    let yAAxis = -(window.innerHeight / 2 - e.screenY)/ 30;
-    card.style.transform = `rotateY(${xAAxis}deg) rotateX(${yAAxis}deg)`
-})
-
-//animation in
-container.addEventListener('mouseenter', (e) => {
-    card.style.transition = `none`
-    price.style.transform = 'translateZ(100px)'
-    title.style.transform = 'translateZ(50px)'
-    features.style.transform = 'translateZ(50px)'
-    button.style.transform = 'translateZ(50px)'
-    
-})
-
-
-
-//animation out
-container.addEventListener('mouseleave', (e) => {
-    card.style.transition = `all .5s ease`
-    card.style.transform = `rotateY(0deg) rotateX(0deg)`
-
-    price.style.transform = 'none'
-    title.style.transform = 'none'
-    button.style.transform = 'none'
-    features.style.transform = 'none'
-
-})
-
-
-
+// MENY REALTED VRIABLES //
 ///Initiation Variables
 var icon_1 = document.getElementById("b1");
 var topLine_1 = document.getElementById("top-line-1");
@@ -118,7 +31,6 @@ var topLeftX_1;
 var topRightX_1;
 var bottomLeftX_1;
 var bottomRightX_1;
-
 ///Animation Variables
 var segmentDuration_1 = 15;
 var menuDisappearDurationInFrames_1 = segmentDuration_1;
@@ -131,7 +43,184 @@ var arrowDisappearComplete_1 = false;
 var menuAppearComplete_1 = false;
 var currentFrame_1 = 1;
 
-///Menu Disappear 
+
+var rellaxx = new rellax('.rellax', {
+    round: true
+});
+
+
+document.querySelector('.activeFAQ').style.paddingBottom = `${document.querySelector('.activeFAQ').children[1].children[0].offsetHeight + 120}px`
+document.querySelectorAll('.faq__card').forEach(el => {
+    el.style.paddingBottom = `${el.children[0].children[0].offsetHeight + 20}px`
+})
+document.querySelector('.faq__content').addEventListener('click', (e) => {
+    if (e.target.closest('.faq__card')){
+        Array.from(document.querySelector('.faq__content').children).forEach(el=>{
+            el.classList.remove('activeFAQ')
+            // el.style.paddingBottom = `0`
+        })
+        e.target.closest('.faq__card').classList.add('activeFAQ')
+        document.querySelectorAll('.faq__card').forEach(el => {
+            el.style.paddingBottom = `${el.children[0].children[0].offsetHeight + 20}px`
+        })
+        document.querySelector('.activeFAQ').style.paddingBottom = `${document.querySelector('.activeFAQ').children[1].children[0].offsetHeight + document.querySelector('.activeFAQ').children[0].offsetHeight + 50}px`
+        
+    }
+})
+
+
+
+
+  ///////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////// EVENTS /////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////
+
+
+///Events
+icon_1.addEventListener( "click", ()=> { 
+  if ( state_1 === "menu" ) {
+      openMenuAnimation_1();
+      responsiveMenuIn()
+    state_1 = "arrow";
+    arrowDisappearComplete_1 = false;
+        menuAppearComplete_1 = false;
+  } else if ( state_1 === "arrow" ) {
+      closeMenuAnimation_1();
+      responsiveMenuOut()
+    state_1 = "menu";
+    menuDisappearComplete_1 = false;
+        arrowAppearComplete_1 = false;
+  }
+});
+
+window.addEventListener('scroll', e => {
+    if (isInViewport(onView)) {
+      imgBack.style.transform = 'translateY(-50%) rotate(0deg)'
+    } else {
+      imgBack.style.transform = 'translateY(-50%) rotate(15deg)'
+    }
+
+})
+
+if (getWidth() > 1024){
+    // mouse animation
+    container.addEventListener('mousemove', (e) => {
+        let xAAxis = -(window.innerWidth / 2 - e.pageX) / 30;
+        let yAAxis = -(window.innerHeight / 2 - e.screenY)/ 30;
+        card.style.transform = `rotateY(${xAAxis}deg) rotateX(${yAAxis}deg)`
+    })
+    
+    //animation in
+    container.addEventListener('mouseenter', (e) => {
+        card.style.transition = `none`
+        price.style.transform = 'translateZ(100px)'
+        title.style.transform = 'translateZ(50px)'
+        features.style.transform = 'translateZ(50px)'
+        button.style.transform = 'translateZ(50px)'
+        
+    })
+    
+    
+    
+    //animation out
+    container.addEventListener('mouseleave', (e) => {
+        card.style.transition = `all .5s ease`
+        card.style.transform = `rotateY(0deg) rotateX(0deg)`
+    
+        price.style.transform = 'none'
+        title.style.transform = 'none'
+        button.style.transform = 'none'
+        features.style.transform = 'none'
+    
+    })
+}    
+
+shakeOnClick.forEach(el => {
+    el.addEventListener('click', e => {
+        el.classList.add('errorShake')
+        setTimeout(() => {
+            el.classList.remove('errorShake')
+        }, 600);
+    })
+    
+})
+
+// document.querySelector('.box').addEventListener('click', (e) => {
+//     closeMenuAnimation_1()
+// })
+
+function responsiveMenuIn() {
+    navList.style.transform = 'translate(-50%,-50%)'
+}
+function responsiveMenuOut() {
+    navList.style.transform = 'translate(100%,-50%)'
+}
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////// SMOOTH SCROLLING WHEN CLICK ////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          };
+        });
+      }
+    }
+  });
+
+  
+  ///////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////// FUNCTIONS //////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////
+  
+  function getWidth() {
+    return Math.max(
+      document.body.scrollWidth,
+      document.documentElement.scrollWidth,
+      document.body.offsetWidth,
+      document.documentElement.offsetWidth,
+      document.documentElement.clientWidth
+    );
+  }
+
+  ///Menu Disappear 
 function menuDisappearAnimation_1() {
     currentFrame_1++;
     if ( currentFrame_1 <= menuDisappearDurationInFrames_1 ) {
@@ -247,75 +336,3 @@ function closeMenuAnimation_1() {
     }
 }
 
-///Events
-icon_1.addEventListener( "click", ()=> { 
-  if ( state_1 === "menu" ) {
-      openMenuAnimation_1();
-      responsiveMenuIn()
-    state_1 = "arrow";
-    arrowDisappearComplete_1 = false;
-        menuAppearComplete_1 = false;
-  } else if ( state_1 === "arrow" ) {
-      closeMenuAnimation_1();
-      responsiveMenuOut()
-    state_1 = "menu";
-    menuDisappearComplete_1 = false;
-        arrowAppearComplete_1 = false;
-  }
-});
-
-
-// document.querySelector('.box').addEventListener('click', (e) => {
-//     closeMenuAnimation_1()
-// })
-
-function responsiveMenuIn() {
-    navList.style.transform = 'translate(-50%,-50%)'
-}
-function responsiveMenuOut() {
-    navList.style.transform = 'translate(100%,-50%)'
-}
-
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          };
-        });
-      }
-    }
-  });
